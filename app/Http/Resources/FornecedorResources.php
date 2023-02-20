68113f6d6ae8db458d0adcc9c7cf7cace1450c83
Lucas-MSF <lucasmasafi@gmail.com>
2023-02-20 16:22:28 -0300

    fix: correção do nome do resource

--- app/Http/Resources/FornecedorResources.php
+++ app/Http/Resources/FornecedorResources.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Fornecedor extends JsonResource
+class FornecedorResources extends JsonResource
 {
     /**
      * Transform the resource into an array.
